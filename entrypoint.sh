1311ecbf87eaa53b9adde676fee15ac1cfee9495
hashmesan <hashmesan@nowhere.com>
2021-07-27 10:50:08 -0400

    clean up

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,11 @@
 #!/bin/sh -l
 
-ls -la
-pwd
 env
-
 cd $GITHUB_WORKSPACE
-ls -la
-
 yarn global add truffle@5.1.67
 yarn global add ganache-cli@6.12.2
 yarn install 
 mv secrets.js.template secrets.js
 tar xvf ganache-db.tar 
-ls -la ganache-db
-sh ganache.sh|tee ganache.log &
+sh ganache.sh|tee ganache.log > /dev/null &
 truffle test
