bd8fcc1f420e07bd7b6f5be76d3dc93b67fa6f5f
hashmesan <hashmesan@nowhere.com>
2021-07-27 15:28:12 -0400

    testing for erc1155 #33

--- test/token_test.js
+++ test/token_test.js
@@ -12,6 +12,7 @@ chai.use(bnChai(BN));
 
 const ERC20SampleToken = artifacts.require("ERC20SampleToken");
 const FungibleToken = artifacts.require("FungibleToken");
+const ERC1155SampleToken = artifacts.require("ERC1155SampleToken");
 
 contract("TokenTest", async (accounts) => {
     var tmpWallet;
@@ -99,6 +100,22 @@ contract("TokenTest", async (accounts) => {
     });
 
     it("should accept HRC1155 and send HRC1155", async () => {
-        
+        var erc1155 = await ERC1155SampleToken.new()
+        const tokenId = 100;
+        await erc1155.mint(accounts[0], tokenId, 10000000);
+
+        //receive: send 100 from accounts[0] -> contract
+        let before = await erc1155.balanceOf(smartWallet.address, tokenId);
+        await erc1155.safeTransferFrom(accounts[0], smartWallet.address, tokenId, 1000, "0x");
+        let after = await erc1155.balanceOf(smartWallet.address, tokenId);
+        expect(after.sub(before)).to.eq.BN(1000);
+
+        // send  send 100 from contract -> receipient
+        before = after;
+        var recipient = web3.eth.accounts.create();
+        const data = erc1155.contract.methods.safeTransferFrom(smartWallet.address, recipient.address, tokenId, 1000, "0x").encodeABI();
+        await multiCall(smartWallet, [{to: erc1155.address, value: 0, data: data}])
+        after = await erc1155.balanceOf(smartWallet.address, tokenId);
+        expect(before.sub(after)).to.eq.BN(1000);
     });
 })
