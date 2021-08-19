863b215f3bd37717987b3321b27fd1a0c980652b
hashmesan <hashmesan@nowhere.com>
2021-08-19 11:33:42 -0400

    relayer query stats

--- relayer/src/web3/transactions.js
+++ relayer/src/web3/transactions.js
@@ -100,6 +100,28 @@ class Transactions {
         var tx = await wallet.executeMetaTx(data.data, data.signatures, data.nonce, data.gasPrice, data.gasLimit, data.refundToken, data.refundAddress, { from: this.defaultAddress, gasLimit: data.gasLimit, nonce: count});
         return {tx: tx}
     }
+
+    async getContractCreated() {
+        const deployedPath = __dirname + "/../../../contracts/deployed.json";
+        let rawdata = fs.readFileSync(deployedPath);
+        let jsonData = JSON.parse(rawdata);
+        const factories = jsonData[this.env];
+        console.log(factories);
+        let res = {};
+        var web3 = new Web3(this.provider);
+
+        for(var i=0; i < factories.length; i++) {
+            var address = factories[i];
+            const factoryContract = new web3.eth.Contract(
+                walletFactoryArtifacts.abi,
+                address
+            );
+            const created = await factoryContract.methods.getCreated().call();
+            res[address] = created.map(e=>{ return {"wallet": e.wallet, "domain": e.domain}})
+        }
+
+        return {"factories": res };
+    }
 }
 
 module.exports = Transactions
