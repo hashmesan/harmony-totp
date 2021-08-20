d54e1aad3cc9028b382710cb5aae23fb2d93919f
Andreas Hejj <andras.hejj@alpina-analytics.com>
2021-08-20 17:04:42 +0200

    updated to bootstrap 5 and cleaned up a bit

--- webclient/src/components/wallet/upgrade.js
+++ webclient/src/components/wallet/upgrade.js
@@ -1,96 +1,134 @@
-import React, { Component } from 'react';
-const {
-	toBech32,
-	fromBech32,
-} = require('@harmony-js/crypto');
+import React, { Component } from "react";
+const { toBech32, fromBech32 } = require("@harmony-js/crypto");
 const web3utils = require("web3-utils");
 import { SmartVaultContext, SmartVaultConsumer } from "../smartvault_provider";
-import RelayerClient from '../../../../lib/relayer_client';
-import Notifications, {notify} from 'react-notify-toast';
+import RelayerClient from "../../../../lib/relayer_client";
+import Notifications, { notify } from "react-notify-toast";
 
 class UpgradePage extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {gasLimit: "", gasPrice:"", gasFee:""}
-	}
+  constructor(props) {
+    super(props);
+    this.state = { gasLimit: "", gasPrice: "", gasFee: "" };
+  }
 
-	componentDidMount() {
-        var self = this;
-        Promise.all([this.context.smartvault.relayClient.getFactoryInfo(), this.context.smartvault.getSmartVaultInfo()]).then(results=>{
-            console.log(results)
+  componentDidMount() {
+    var self = this;
+    Promise.all([
+      this.context.smartvault.relayClient.getFactoryInfo(),
+      this.context.smartvault.getSmartVaultInfo(),
+    ]).then((results) => {
+      console.log(results);
 
-            self.setState({
-                gasLimit: self.context.smartvault.config.gasLimit,
-                gasPrice: web3utils.fromWei(self.context.smartvault.config.gasPrice + "", "gwei"),
-                gasFee: web3utils.toBN(self.context.smartvault.config.gasLimit).mul(web3utils.toBN(self.context.smartvault.config.gasPrice)),
-                contractInfo: results[1],
-                factoryInfo: results[0],
-                different: results[0].implementation != results[1].masterCopy
-            });    
-        })
-	}
+      self.setState({
+        gasLimit: self.context.smartvault.config.gasLimit,
+        gasPrice: web3utils.fromWei(
+          self.context.smartvault.config.gasPrice + "",
+          "gwei"
+        ),
+        gasFee: web3utils
+          .toBN(self.context.smartvault.config.gasLimit)
+          .mul(web3utils.toBN(self.context.smartvault.config.gasPrice)),
+        contractInfo: results[1],
+        factoryInfo: results[0],
+        different: results[0].implementation != results[1].masterCopy,
+      });
+    });
+  }
 
-	transfer(e) {
-		e.preventDefault();
-		var self = this;
-		self.setState({ submitting: true });
+  transfer(e) {
+    e.preventDefault();
+    var self = this;
+    self.setState({ submitting: true });
 
-        var methodData = RelayerClient.getContract().methods.upgradeMasterCopy(this.state.factoryInfo.implementation).encodeABI();    
-        this.context.smartvault.submitTransaction(methodData).then(e => {
-			setTimeout(() => {
-				self.setState({ submitting: false });
-				notify.show('Transaction Successful!');
-				window.location.reload
-			}, 3000);
-		}).catch(e => {
-			console.error(e);
-			self.setState({ submitting: false, error: e });
-		})
-	}
+    var methodData = RelayerClient.getContract()
+      .methods.upgradeMasterCopy(this.state.factoryInfo.implementation)
+      .encodeABI();
+    this.context.smartvault
+      .submitTransaction(methodData)
+      .then((e) => {
+        setTimeout(() => {
+          self.setState({ submitting: false });
+          notify.show("Transaction Successful!");
+          window.location.reload;
+        }, 3000);
+      })
+      .catch((e) => {
+        console.error(e);
+        self.setState({ submitting: false, error: e });
+      });
+  }
 
-	updateFee() {
-		this.setState({ gasFee: web3utils.toBN(this.state.gasLimit).mul(web3utils.toBN(web3utils.toWei(this.state.gasPrice, 'gwei'))) })
-	}
+  updateFee() {
+    this.setState({
+      gasFee: web3utils
+        .toBN(this.state.gasLimit)
+        .mul(web3utils.toBN(web3utils.toWei(this.state.gasPrice, "gwei"))),
+    });
+  }
 
-	render() {
-		return (
-			<SmartVaultConsumer>
-                {({smartvault}) => (				
-			<div className="card">
-				<div className="card-body">
-					<h3 className="card-title text-center">Upgrade</h3>
-					<form class="text-center mt-3">
-						<div className="form-group">
-                            <div>Current Version: {this.state.contractInfo && this.state.contractInfo.masterCopy}</div>
-                            <div>Latest Version: {this.state.factoryInfo && this.state.factoryInfo.implementation}</div>
-						</div>
-						<div className="form-group row">
-                            <pre className="col-12">
-                                {this.state.factoryInfo && this.state.factoryInfo.releaseNotes}
-                            </pre>
-						</div>
-						{this.state.error &&
-							<div className="row justify-content-md-center mt-4">
-								<div className="alert alert-danger w-50" role="alert">
-									{this.state.error && JSON.stringify(this.state.error)}
-								</div>
-							</div>}
-						<div className="form-group row mt-4">
-                            {this.state.different
-                            ?
-							<div className="col-sm-12">
-								{!this.state.submitting && <button className="btn btn-primary" onClick={this.transfer.bind(this)}>Upgrade</button>}
-								{this.state.submitting && <button className="btn btn-primary" disabled>Submitting..(wait)</button>}
-							</div>
-                            : 
-                            <div className="col-sm-12 text-center"><b>Your contract is using the latest version</b></div>}
-						</div>
-					</form>
-				</div>
-			</div>)}
-			</SmartVaultConsumer>
-		);
-	}
+  render() {
+    return (
+      <SmartVaultConsumer>
+        {({ smartvault }) => (
+          <div className="card">
+            <div className="card-body">
+              <h3 className="card-title text-center">Upgrade</h3>
+              <form className="text-center mt-3">
+                <div className="form-group">
+                  <div>
+                    Current Version:{" "}
+                    {this.state.contractInfo &&
+                      this.state.contractInfo.masterCopy}
+                  </div>
+                  <div>
+                    Latest Version:{" "}
+                    {this.state.factoryInfo &&
+                      this.state.factoryInfo.implementation}
+                  </div>
+                </div>
+                <div className="form-group row">
+                  <pre className="col-12">
+                    {this.state.factoryInfo &&
+                      this.state.factoryInfo.releaseNotes}
+                  </pre>
+                </div>
+                {this.state.error && (
+                  <div className="row justify-content-md-center mt-4">
+                    <div className="alert alert-danger w-50" role="alert">
+                      {this.state.error && JSON.stringify(this.state.error)}
+                    </div>
+                  </div>
+                )}
+                <div className="form-group row mt-4">
+                  {this.state.different ? (
+                    <div className="col-sm-12">
+                      {!this.state.submitting && (
+                        <button
+                          className="btn btn-primary"
+                          onClick={this.transfer.bind(this)}
+                        >
+                          Upgrade
+                        </button>
+                      )}
+                      {this.state.submitting && (
+                        <button className="btn btn-primary" disabled>
+                          Submitting..(wait)
+                        </button>
+                      )}
+                    </div>
+                  ) : (
+                    <div className="col-sm-12 text-center">
+                      <b>Your contract is using the latest version</b>
+                    </div>
+                  )}
+                </div>
+              </form>
+            </div>
+          </div>
+        )}
+      </SmartVaultConsumer>
+    );
+  }
 }
 UpgradePage.contextType = SmartVaultContext;
 
--- webclient/src/index.js
+++ webclient/src/index.js
@@ -1,60 +1,55 @@
-import React, { Component } from 'react';
-var ReactDOM = require('react-dom');
+import React, { Component } from "react";
+var ReactDOM = require("react-dom");
 
 import {
-    HashRouter as Router,
-    Switch,
-    Route,
-    Redirect,
-    Link
-  } from "react-router-dom";
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
-import createStore from "redux-zero";
 import { Provider, connect } from "redux-zero/react";
-import {getLocalWallet} from "./config";
+import { getLocalWallet } from "./config";
 
-import store from './redux/store';
-import Header from './components/header';
-import Create from './components/create';
-import Wallet from './components/wallet';
-import Recover from './components/recover';
+import store from "./redux/store";
+import Header from "./components/header";
+import Create from "./components/create";
+import Wallet from "./components/wallet";
+import Recover from "./components/recover";
 
 const mapToProps = ({ environment }) => ({ environment });
-const App = connect(mapToProps)(({environment}) => (
-    <Router>
-        <Header/>
-        <Switch>
-            <Route exact path="/">
-                {getLocalWallet(environment, false) ?<Redirect to="/wallet"/> : <Redirect to="/create"/>}
-            </Route>
-            <Route path="/create">
-                <Create/>
-            </Route>
-            <Route path="/wallet">
-                <Wallet/>
-            </Route>
-            <Route path="/recover">
-                <Recover/>
-            </Route>
-        </Switch>
-        <footer className="my-5 pt-5 text-muted text-center text-small">
-            <p className="mb-1">Smartvault (beta) powered by Harmony Blockchain | Opensource (GPL)</p>
-            <ul className="list-inline">
-            <li className="list-inline-item"><a href="https://github.com/hashmesan/harmony-totp">Github</a></li>
-            <li className="list-inline-item"><a href="https://github.com/hashmesan/harmony-totp/issues">Report issues</a></li>
-            </ul>
-        </footer>
-    </Router>    
-))
+const App = connect(mapToProps)(({ environment }) => (
+  <Router>
+    <Header />
+    <Switch>
+      <Route exact path="/">
+        {getLocalWallet(environment, false) ? (
+          <Redirect to="/wallet" />
+        ) : (
+          <Redirect to="/create" />
+        )}
+      </Route>
+      <Route path="/create">
+        <Create />
+      </Route>
+      <Route path="/wallet">
+        <Wallet />
+      </Route>
+      <Route path="/recover">
+        <Recover />
+      </Route>
+    </Switch>
+  </Router>
+));
 
 class MainScreen extends Component {
-    render() {
-        return (
-            <Provider store={store}>
-                <App/>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
-            </Provider>
-        );
-    }
+  render() {
+    return (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  }
 }
 
-ReactDOM.render(<MainScreen/>, document.getElementById('container'));
+ReactDOM.render(<MainScreen />, document.getElementById("container"));
