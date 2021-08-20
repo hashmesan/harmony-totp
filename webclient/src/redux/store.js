3f0208c2b3f89d9eef5b410c8434a66be4bf1997
Andreas Hejj <andras.hejj@alpina-analytics.com>
2021-08-20 17:09:20 +0200

    updated gitignore

--- webclient/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import createStore from "redux-zero";
-import CONFIG from "../config";
-
-const initialState = { environment: localStorage.getItem("environment") || "mainnet0", config: CONFIG[localStorage.getItem("environment") || "mainnet0"]};
-const store = createStore(initialState);
-
-export default store;
-
