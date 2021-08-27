e54f3b46e6a9124f8862b52f3007622ecbba35ef
hashmesan <hashmesan@nowhere.com>
2021-08-27 17:43:58 -0400

    get transactions call fix, and formatting

--- webclient/src/components/wallet/transactions.js
+++ webclient/src/components/wallet/transactions.js
@@ -1,23 +1,42 @@
 import axios from 'axios';
 import React, { Component } from 'react';
+const web3utils = require("web3-utils");
+import moment from 'moment';
 
 const TransactionRow = ({data, me})=> {
-    if(data.function.name == "setHashStorageId") {
-        return (<div>
+    if(!data.function) {
+        return (<div className="mb-2">
             <div>
-                <div>{data.function.name} on {new Date(data.timestamp*1000).toISOString()}</div>
-                <div className="">{data.function.params.id}</div>                
+                <div><b>RECEIVED</b> {web3utils.fromWei(data.value)} ONE <span className="float-right">{moment(new Date(data.timestamp*1000)).fromNow(true)}</span></div>
+                <div className="">From {data.from}<span className="float-right">Status: {Number(data.returnData.success)}</span></div>                
+            </div> 
+        </div>)
+    }
+    if(data.function.name == "setHashStorageId") {
+        return (<div className="mb-2">
+            <div>
+                <div><b>{data.function.name}</b><span className="float-right">{moment(new Date(data.timestamp*1000)).fromNow(true)}</span></div>
+                <div className="">{data.function.params.id}<span className="float-right">Status: {Number(data.returnData.success)}</span></div>                
             </div> 
         </div>)
     }
     if(data.function.name == "makeTransfer") {
-        return (<div>
+        return (<div className="mb-2">
             <div>
-                <div>{data.function.name} to {data.function.params.to} on {new Date(data.timestamp*1000).toISOString()}</div>
+                <div>{data.function.name} to {data.function.params.to} <span className="float-right">{moment(new Date(data.timestamp*1000)).fromNow(true)}</span></div>
                 <div className="">Status: {Number(data.returnData.success)} ({data.returnData.error})</div>                
             </div> 
         </div>)
+    } else {
+        return (<div className="mb-2">
+            <div>
+                <div><b>{data.function.name}</b> <span className="float-right">{moment(new Date(data.timestamp*1000)).fromNow(true)}</span></div>
+                <div className="">&nbsp;<span className="float-right">Status: {Number(data.returnData.success)}</span></div>                
+            </div> 
+        </div>)        
     }
+
+    console.log(data)
 
     return null;
 }
