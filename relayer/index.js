bbe55709bf8088f71e1c5dc5cffb4726c34f2ab7
Andreas Hejj <andreas.hejj@gmail.com>
2021-09-22 15:13:47 +0200

    updated port naming for heroku

--- relayer/index.js
+++ relayer/index.js
@@ -3,8 +3,10 @@ const server = require("./src/adapters/express");
 const serverless = require("./src/adapters/serverless");
 
 module.exports = {
-  server: server.init(createRequest, process.env.EA_PORT || 8989),
+  server: server.init(createRequest, process.env.PORT || 8989),
   gcpservice: serverless.initGcpService(createRequest),
   handler: serverless.initHandler(createRequest),
   handlerv2: serverless.initHandlerV2(createRequest),
 };
+
+//server: server.init(createRequest, process.env.EA_PORT || 8989),
